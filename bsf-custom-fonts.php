683b23481ff6fdd6e0febb3198bf8d374fe04885
Nikhil Chavan <email@nikhilchavan.com>
2017-08-09 11:57:27 +0530

    Scaffold initial plugin files

---
+++ bsf-custom-fonts.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Bsf Custom Fonts
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     bsf-custom-fonts
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bsf_Custom_Fonts
+ */
