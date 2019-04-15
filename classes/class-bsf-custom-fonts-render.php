3aea9fbb6e5a8ef1a3e9cf7a7ea953321efff156
Uttam Sharma <uttams@bsf.io>
2019-04-15 17:34:54 +0530

    fixed phpcs

--- classes/class-bsf-custom-fonts-render.php
+++ classes/class-bsf-custom-fonts-render.php
@@ -181,7 +181,7 @@ if ( ! class_exists( 'Bsf_Custom_Fonts_Render' ) ) :
 
 			foreach ( $fonts as $font => $links ) {
 
-				$font_value = "'".$font."'";
+				$font_value = "'" . $font . "'";
 				if ( empty( $links['font_fallback'] ) && '' == $links['font_fallback'] ) {
 					echo '<option value="' . self::get_font_values( $font_value, $links['font_fallback'] ) . '" ' . selected( $font, $value, false ) . '>' . esc_attr( $font ) . '</option>';
 				} else {
