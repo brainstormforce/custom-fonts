b40f9f011343676e7f0d2905840444fef3e32d19
imnavanath <navanathb@bsf.io>
2020-06-09 16:48:27 +0530

    feat: BSF Analytics trcker added

--- custom-fonts.php
+++ custom-fonts.php
@@ -6,7 +6,7 @@
  * Author:          Brainstorm Force
  * Author URI:      http://www.brainstormforce.com
  * Text Domain:     custom-fonts
- * Version:         1.2.1
+ * Version:         1.2.2
  *
  * @package         Bsf_Custom_Fonts
  */
@@ -25,9 +25,12 @@ define( 'BSF_CUSTOM_FONTS_FILE', __FILE__ );
 define( 'BSF_CUSTOM_FONTS_BASE', plugin_basename( BSF_CUSTOM_FONTS_FILE ) );
 define( 'BSF_CUSTOM_FONTS_DIR', plugin_dir_path( BSF_CUSTOM_FONTS_FILE ) );
 define( 'BSF_CUSTOM_FONTS_URI', plugins_url( '/', BSF_CUSTOM_FONTS_FILE ) );
-define( 'BSF_CUSTOM_FONTS_VER', '1.2.1' );
+define( 'BSF_CUSTOM_FONTS_VER', '1.2.2' );
 
 /**
  * BSF Custom Fonts
  */
 require_once BSF_CUSTOM_FONTS_DIR . 'classes/class-bsf-custom-fonts.php';
+
+// BSF Analytics library.
+require_once BSF_CUSTOM_FONTS_DIR . 'admin/bsf-analytics/class-bsf-analytics.php';
