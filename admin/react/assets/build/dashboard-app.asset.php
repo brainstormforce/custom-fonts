af5c7f81e9cf81d06249bfda4c53279d22bc8a83
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-06-16 19:11:25 +0530

    improv: clear search input

--- admin/react/assets/build/dashboard-app.asset.php
+++ admin/react/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7f3e69eeb5894d5ade94');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5dc94e78b27c06b9872f');
--- admin/react/assets/build/dashboard-app.js
+++ admin/react/assets/build/dashboard-app.js
@@ -2696,6 +2696,8 @@ const SearchBar = _ref => {
   } = _ref;
   const [query, setQuery] = (0,react__WEBPACK_IMPORTED_MODULE_2__.useState)("");
   const debouncedQuery = (0,_utils_useDebounce__WEBPACK_IMPORTED_MODULE_4__["default"])(query, 800);
+  const [searchIcon, setSearchIcon] = (0,react__WEBPACK_IMPORTED_MODULE_2__.useState)(true);
+  const ref = (0,react__WEBPACK_IMPORTED_MODULE_2__.useRef)(null);
   const handleSearch = e => {
     const q = e.target.value;
     setLoading(true);
@@ -2711,25 +2713,53 @@ const SearchBar = _ref => {
       }
     });
   }, [debouncedQuery]);
+  const loadSearchIcon = () => {
+    if (searchIcon) {
+      return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
+        width: "14",
+        height: "14",
+        viewBox: "0 0 14 14",
+        fill: "none",
+        xmlns: "http://www.w3.org/2000/svg",
+        className: "mr-2"
+      }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+        fillRule: "evenodd",
+        clipRule: "evenodd",
+        d: "M6.19961 1.79998C3.76956 1.79998 1.79961 3.76992 1.79961 6.19998C1.79961 8.63003 3.76956 10.6 6.19961 10.6C7.4148 10.6 8.51398 10.1081 9.31088 9.31124C10.1078 8.51434 10.5996 7.41517 10.5996 6.19998C10.5996 3.76992 8.62966 1.79998 6.19961 1.79998ZM0.599609 6.19998C0.599609 3.10718 3.10681 0.599976 6.19961 0.599976C9.2924 0.599976 11.7996 3.10718 11.7996 6.19998C11.7996 7.52999 11.3354 8.75242 10.561 9.71283L13.2239 12.3757C13.4582 12.61 13.4582 12.9899 13.2239 13.2242C12.9896 13.4586 12.6097 13.4586 12.3753 13.2242L9.71246 10.5614C8.75206 11.3357 7.52963 11.8 6.19961 11.8C3.10681 11.8 0.599609 9.29277 0.599609 6.19998Z",
+        fill: "#7E7E7E"
+      }));
+    } else {
+      return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("button", {
+        type: "button",
+        onClick: () => {
+          // Clear input.
+          ref.current.value = "";
+          setSearchIcon(true);
+        }
+      }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
+        width: "20",
+        height: "20",
+        viewBox: "0 0 20 20",
+        fill: "none",
+        className: "mr-2"
+      }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+        d: "M5 15L15 5M5 5L15 15",
+        stroke: "#9CA3AF",
+        strokeWidth: "2",
+        strokeLinecap: "round",
+        strokeLinejoin: "round"
+      })));
+    }
+  };
   return (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
     className: "relative w-full my-6 flex items-center border-b border-light bsf-custom-font-search"
-  }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
-    width: "14",
-    height: "14",
-    viewBox: "0 0 14 14",
-    fill: "none",
-    xmlns: "http://www.w3.org/2000/svg",
-    className: "mr-2"
-  }, (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
-    fillRule: "evenodd",
-    clipRule: "evenodd",
-    d: "M6.19961 1.79998C3.76956 1.79998 1.79961 3.76992 1.79961 6.19998C1.79961 8.63003 3.76956 10.6 6.19961 10.6C7.4148 10.6 8.51398 10.1081 9.31088 9.31124C10.1078 8.51434 10.5996 7.41517 10.5996 6.19998C10.5996 3.76992 8.62966 1.79998 6.19961 1.79998ZM0.599609 6.19998C0.599609 3.10718 3.10681 0.599976 6.19961 0.599976C9.2924 0.599976 11.7996 3.10718 11.7996 6.19998C11.7996 7.52999 11.3354 8.75242 10.561 9.71283L13.2239 12.3757C13.4582 12.61 13.4582 12.9899 13.2239 13.2242C12.9896 13.4586 12.6097 13.4586 12.3753 13.2242L9.71246 10.5614C8.75206 11.3357 7.52963 11.8 6.19961 11.8C3.10681 11.8 0.599609 9.29277 0.599609 6.19998Z",
-    fill: "#7E7E7E"
-  })), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("input", {
+  }, loadSearchIcon(), (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.createElement)("input", {
     className: "w-full border-0",
     type: "text",
     placeholder: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_1__.__)("Search Custom Font", "custom-fonts"),
-    onChange: handleSearch
+    onChange: handleSearch,
+    onFocus: () => setSearchIcon(false),
+    ref: ref
   }));
 };
 /* harmony default export */ __webpack_exports__["default"] = (SearchBar);
