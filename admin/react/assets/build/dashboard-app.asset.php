1eeaef079249ccab54713ca768aca801ac7159b1
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-06-16 19:12:04 +0530

    chore: build

--- admin/react/assets/build/dashboard-app.asset.php
+++ admin/react/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5dc94e78b27c06b9872f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '801f8013b17069c9ffd7');
