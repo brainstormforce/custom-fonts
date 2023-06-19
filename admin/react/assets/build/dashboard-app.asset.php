301825732741fd5c25633bc768c9ba8c29037485
Navanath Bhosale <navanathb@bsf.io>
2023-06-19 18:06:35 +0530

    chore: added spacing for notice wrappers

--- admin/react/assets/build/dashboard-app.asset.php
+++ admin/react/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2c4303f4532f9ca479a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f3e01ac5ce08c6d9f0bc');
