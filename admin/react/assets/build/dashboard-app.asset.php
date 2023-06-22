b9112ddb57a070456f2f30ab9099d424288b7c28
Navanath Bhosale <navanathb@bsf.io>
2023-06-22 13:20:04 +0530

    fix: directory not created again if deletes with classic theme

--- admin/react/assets/build/dashboard-app.asset.php
+++ admin/react/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '74879346fa719078d199');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ed65b87f0006012961c4');
