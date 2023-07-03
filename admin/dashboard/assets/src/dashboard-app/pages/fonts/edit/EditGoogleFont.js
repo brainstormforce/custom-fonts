ed03f8a645dd36c50622f8b9f2e6d19abe3cf955
yssambare12 <bloggingscan@gmail.com>
2023-07-03 16:54:35 +0530

    chore:updated files

--- admin/dashboard/assets/src/dashboard-app/pages/fonts/edit/EditGoogleFont.js
+++ admin/dashboard/assets/src/dashboard-app/pages/fonts/edit/EditGoogleFont.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { __ } from "@wordpress/i18n";
 import apiFetch from '@wordpress/api-fetch';
 import { useSelector } from 'react-redux';
+import Custom_Fonts_Icons from "@Common/svg-icons";
 
 const EditGoogleVariationItem = ({
 	id,
@@ -143,12 +144,7 @@ const EditGoogleFont = ({fontId, fontName}) => {
 							disabled={'loading' === isLoading ? true : false}
 						>
 							{__( 'Save Font', 'custom-fonts' )}
-							{ 'loading' === isLoading && (
-								<svg className="animate-spin -mr-1 ml-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-									<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-									<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-								</svg>
-							) }
+							{ 'loading' === isLoading && Custom_Fonts_Icons['saveFont']}
 						</button>
 					</div>
 				</div>
