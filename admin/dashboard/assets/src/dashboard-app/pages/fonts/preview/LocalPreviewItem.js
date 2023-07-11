cfcd0beaf23f97b96c9777b08e31e7fd0fb16d33
yssambare12 <bloggingscan@gmail.com>
2023-07-11 08:57:47 +0530

    chore: updated localpreviewitem.js file

--- admin/dashboard/assets/src/dashboard-app/pages/fonts/preview/LocalPreviewItem.js
+++ admin/dashboard/assets/src/dashboard-app/pages/fonts/preview/LocalPreviewItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from 'react-redux';
 import { __ } from "@wordpress/i18n";
 import LFontVariation from './LFontVariation';
+import {getLocalFontStyle} from '../../../../common/GlobalFunctions';
 
 const LocalPreviewItem = () => {
 	const localFont = useSelector( ( state ) => state.localFont );
@@ -35,39 +36,12 @@ const LocalPreviewItem = () => {
 		return previewNameToggleCSS;
 	}
 
-	const getLocalFontStyle = () => {
-		let defaultFont = `@font-face {\r\n\tfont-family: '${fontName}';`;
-		let srcFont = '';
-
-		variations.map((variation) => {
-			let fontUrl = variation.font_url,
-				weight = variation.font_weight,
-				style = '' === variation.font_style ? 'normal' : variation.font_style,
-				src = 'url(\'' + fontUrl + '\') ';
-			if (fontUrl.includes('.woff2')) {
-				src += 'format(\'woff2\')';
-			} else if (fontUrl.includes('.woff')) {
-				src += 'format(\'woff\')';
-			} else if (fontUrl.includes('.svg')) {
-				src += 'format(\'svg\')';
-			} else if (fontUrl.includes('.ttf')) {
-				src += 'format(\'truetype\')';
-			} else if (fontUrl.includes('.otf')) {
-				src += 'format(\'OpenType\')';
-			} else if (fontUrl.includes('.eot')) {
-				src = 'url(\'' + fontUrl + '?#iefix\') format(\'embedded-opentype\')';
-			} else {
-				// Do nothing.
-			}
-			srcFont += `${defaultFont}\r\n\tfont-style: ${style};\r\n\tfont-weight: ${weight};\r\n\tsrc: ${src};\r\n}\r\n`;
-		});
-
-		return srcFont;
-	}
+	console.log(fontName)
+	console.log("@@@@@@@@@@@@@@@@@")
 
 	return (
 		<div className="local-fonts-preview-wrapper">
-			<style id={`bcf-local-font-css`}> {`${getLocalFontStyle()} ${getPreviewTitleToggleCSS()}` } </style>
+			<style id={`bcf-local-font-css`}> {`${getLocalFontStyle(fontName, variations)} ${getPreviewTitleToggleCSS()}` } </style>
 			{
 				variations && variations.map( ( variation ) => (
 					<LFontVariation font={fontName} weight={variation.font_weight} fontUrl={variation.font_url} style={ '' === variation.font_style ? 'normal' : variation.font_style } key={parseInt(variation.font_weight)+1000} />
