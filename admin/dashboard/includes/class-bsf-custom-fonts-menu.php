584a39f81124fb9b252c54be4fa750c0c1bb51e2
Navanath Bhosale <navanathb@bsf.io>
2023-07-11 12:31:19 +0530

    chore: pot updated

--- admin/dashboard/includes/class-bsf-custom-fonts-menu.php
+++ admin/dashboard/includes/class-bsf-custom-fonts-menu.php
@@ -276,7 +276,6 @@ class BSF_Custom_Fonts_Menu {
 		$footer_text = sprintf(
 			/* translators: 1: Custom Fonts, 2: Plugin rating link */
 			__( 'Enjoyed %1$s? Please leave us a %2$s rating. We really appreciate your support!', 'custom-fonts' ),
-			/* translators: Plugin brand name, do not translate. */
 			'<span class="bcf-footer-thankyou"><strong> Custom Fonts </strong>',
 			'<a href="https://wordpress.org/support/plugin/custom-fonts/reviews/?filter=5#new-post" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a></span>'
 		);
