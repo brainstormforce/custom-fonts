ed7a3a17d94b59c42de9a29b560d0cf2a4b5dd3b
Yogesh <bloggingscan@gmail.com>
2023-07-28 08:15:59 +0530

    chore:removed popup window save button

--- admin/dashboard/assets/src/dashboard-app/pages/fonts/edit/EditGoogleFont.js
+++ admin/dashboard/assets/src/dashboard-app/pages/fonts/edit/EditGoogleFont.js
@@ -135,23 +135,6 @@ const EditGoogleFont = ({fontId, fontName}) => {
 							</div>
 						</div>
 					</div>
-
-					{/* <div className="my-5">
-						<button
-							type="button"
-							className="bcf-save-font inline-flex components-button is-primary mb-5"
-							onClick={ updatingNewFontPost }
-							disabled={'loading' === isLoading ? true : false}
-						>
-							{__( 'Save Font', 'custom-fonts' )}
-							{ 'loading' === isLoading && (
-								<svg className="animate-spin -mr-1 ml-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-									<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-									<path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-								</svg>
-							) }
-						</button>
-					</div> */}
 				</div>
 			</div>
 		</div>
