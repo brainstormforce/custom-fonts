70589ea98c24d92a1a24ee2dcfafb14b429b93ee
Yogesh <bloggingscan@gmail.com>
2023-08-16 17:26:34 +0530

    chore:svg colour fixes

--- admin/dashboard/assets/src/dashboard-app/pages/fonts/edit/preview/EditGooglePreviewItem.js
+++ admin/dashboard/assets/src/dashboard-app/pages/fonts/edit/preview/EditGooglePreviewItem.js
@@ -147,7 +147,7 @@ const EditGFontVariation = (
                   ></circle>
                   <path
                     className="opacity-75"
-                    fill="rgb(230 80 84)"
+                    fill="#3858E9"
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   ></path>
                 </svg>
@@ -214,7 +214,7 @@ const EditGFontVariation = (
                   ></circle>
                   <path
                     className="opacity-75"
-                    fill="rgb(0, 124, 186)"
+                    fill="#3858E9"
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   ></path>
                 </svg>
