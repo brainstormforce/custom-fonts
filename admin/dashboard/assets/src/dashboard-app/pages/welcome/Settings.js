a0569bed0ef580e902e9dfdd4f32e5b71be74709
Navanath Bhosale <navanathb@bsf.io>
2023-08-19 12:21:43 +0530

    refactor: updated toggle switch

--- admin/dashboard/assets/src/dashboard-app/pages/welcome/Settings.js
+++ admin/dashboard/assets/src/dashboard-app/pages/welcome/Settings.js
@@ -44,17 +44,23 @@ const Settings = () => {
 						<Switch
 							checked={isChecked}
 							onChange={preloadingUpdate}
-							className={classNames(
-							isChecked ? 'bg-indigo-600' : 'bg-gray-200',
-							'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2'
-							)}
-						>
+							className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-[#3858e9] focus:ring-offset-2"
+							>
+							<span className="sr-only">Use setting</span>
+							<span aria-hidden="true" className="pointer-events-none absolute h-full w-full rounded-md bg-white" />
 							<span
-							aria-hidden="true"
-							className={classNames(
-								isChecked ? 'translate-x-5' : 'translate-x-0',
-								'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
-							)}
+								aria-hidden="true"
+								className={classNames(
+									isChecked ? 'bg-[#3858e9]' : 'bg-gray-200',
+									'pointer-events-none absolute mx-auto h-4 w-9 rounded-full transition-colors duration-200 ease-in-out'
+								)}
+							/>
+							<span
+								aria-hidden="true"
+								className={classNames(
+									isChecked ? 'translate-x-5' : 'translate-x-0',
+									'pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out'
+								)}
 							/>
 						</Switch>
 						<Switch.Label as="span" className="ml-3 text-sm switch-toggle-label">
