0d1ae73ac32bd80ad7fa24191b51be03e1b3db18
Yogesh <bloggingscan@gmail.com>
2023-08-23 09:18:37 +0530

    chore:phpcs fixes

--- includes/class-bcf-google-fonts-compatibility.php
+++ includes/class-bcf-google-fonts-compatibility.php
@@ -165,9 +165,9 @@ if ( ! class_exists( 'BCF_Google_Fonts_Compatibility' ) ) {
 							);
 						}
 
-						$final_font_files_flat = array_reduce( $final_font_files, function ( $carry, $item ) {
-							return array_merge( $carry, is_array( $item ) ? $item : [ $item ] );
-						}, array() );
+						$final_font_files_flat = array_reduce($final_font_files, function ($carry, $item) {
+							return array_merge($carry, is_array($item) ? $item : [$item]);
+						}, array());						
 
 						// Add each variant as one font face.
 						$new_font_faces[] = array(
