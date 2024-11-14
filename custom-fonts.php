6a3d82cc492cc953c8facbf30431ef5edd2d005f
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2024-11-14 12:12:34 +0530

    chore:release pr

--- custom-fonts.php
+++ custom-fonts.php
@@ -25,7 +25,7 @@ define( 'BSF_CUSTOM_FONTS_FILE', __FILE__ );
 define( 'BSF_CUSTOM_FONTS_BASE', plugin_basename( BSF_CUSTOM_FONTS_FILE ) );
 define( 'BSF_CUSTOM_FONTS_DIR', plugin_dir_path( BSF_CUSTOM_FONTS_FILE ) );
 define( 'BSF_CUSTOM_FONTS_URI', plugins_url( '/', BSF_CUSTOM_FONTS_FILE ) );
-define( 'BSF_CUSTOM_FONTS_VER', '2.1.7' );
+define( 'BSF_CUSTOM_FONTS_VER', '2.1.8' );
 define( 'BSF_CUSTOM_FONTS_POST_TYPE', 'bsf_custom_fonts' );
 define( 'BSF_CUSTOM_FONTS_ADMIN_PAGE', 'bsf-custom-fonts' );
 
